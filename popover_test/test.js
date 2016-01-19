e47b976605946f2469d0e036e66ed9c592fb0dce
madewithlinux <madewithlinux.josh@gmail.com>
2016-01-18 21:26:29 -0600

    updated the age pop-over according to new design

--- popover_test/test.js
+++ popover_test/test.js
@@ -1,41 +1,26 @@
-function handle_popup() {
-    var verified = false;
-    if (document.getElementById("answer_yes").checked) {
-        /*yes is checked, so the user is old enough*/
-        document.body.innerHTML += '' +
-            '<style type="text/css">' +
-            'div.popover {' +
-            'display: none;' +
-            '}' +
-            '</style>';
-        document.cookie = "age_verified_v2=yes;max-age=" + (10 * 365 * 24 * 60 * 60);
-    } else if (document.getElementById("answer_no").checked) {
-        /*no is checked, redirect the user somewhere else*/
-        window.location.href = "http://pbskids.org/";
-    }
+function hide_popup() {
+    document.body.innerHTML += '' +
+        '<style type="text/css">' +
+        'div.popover {' +
+        'display: none;' +
+        '}' +
+        '</style>';
+    document.cookie = "age_verified_v2=yes;max-age=" + (10 * 365 * 24 * 60 * 60);
 }
 function do_age_verification() {
     var age_verified = (document.cookie.indexOf('age_verified_v2') != -1);
     if (!age_verified) {
         document.body.innerHTML += '' +
+            '' +
             '<div class="popover" id="popover">' +
             '<div class="prompt">' +
             '<p class="question">' +
-            '<img align="middle" src="files/theme/popover_logo.png"> <br>' +
-            '<img align="middle" src="files/theme/popover_are_you_21.png"> <br>' +
-            '</p>' +
-            '<div id="input_form">' +
-            '<input type="radio" name="answer" id="answer_yes">' +
-            '<label for="answer_yes">' +
-            '<img align="middle" class="btn" src="files/theme/popover_yes.png"> <br>' +
-            '</label>' +
-            '<input type="radio" name="answer" id="answer_no">' +
-            '<label for="answer_no">' +
-            '<img align="middle" class="btn" src="files/theme/popover_no.png"> <br>' +
-            '</label>' +
-            '</div>' +
-            '<p class="question">' +
-            '<img align="middle" id="button_submit" onclick="handle_popup()" src="files/theme/popover_enter_button.png">' +
+            '<img align="middle" class="logo" src="files/theme/popover_logo.png">' +
+            '<br>' +
+            '<img align="middle" class="question" src="files/theme/popover_are_you_21.png">' +
+            '<br>' +
+            '<img align="left" class="btn" src="files/theme/popover_yes.png" onclick="hide_popup()">' +
+            '<a href="http://pbskids.org"><img align="right" class="btn" src="files/theme/popover_no.png"></a>' +
             '</p>' +
             '</div>' +
             '</div>' +
@@ -58,7 +43,7 @@ function do_age_verification() {
             'width: 30%;' +
             'left: 35%;' +
             'top: 20%;' +
-            'line-height: 40px;' +
+            'line-height: 5em;' +
             'padding: 10px;' +
             'background-color: #5d5d5d;' +
             'border: 2px solid #000;' +
@@ -66,61 +51,45 @@ function do_age_verification() {
             '}' +
             '' +
             'div.popover div.prompt p {' +
-            'margin-bottom: 0;' +
+            'margin: 0;' +
+            'padding: 20px;' +
             '}' +
             '' +
-            'div.popover div.prompt img {' +
-            'margin-bottom: 20px;' +
-            'width: 70%;' +
+            'div.popover div.prompt img.logo {' +
+            'width: 100%;' +
             '}' +
             '' +
-            'div.popover div.prompt .question {' +
+            'div.popover div.prompt img.question {' +
+            'height: 1.5em;' +
+            'width: auto;' +
+            '}' +
+            '' +
+            'div.popover div.prompt img.btn {' +
+            'margin-top: 10px;' +
+            'margin-bottom: 10px;' +
+            'height: 1.5em;' +
+            'width: auto;' +
+            '}' +
+            '' +
+            'div.popover div.prompt p.question {' +
             'text-align: center;' +
             'font-size: 1.5em;' +
             'color: #fff;' +
             '}' +
             '' +
-            'div.popover div.prompt div#input_form {' +
-            'line-height: 30px;' +
-            'width: 120px;' +
-            'margin: auto;' +
+            '@media screen and (max-width: 600px) {' +
+            'div.popover div.prompt {' +
+            'position: fixed;' +
+            'width: 90vw;' +
+            'margin-left: auto;' +
+            'margin-right: auto;' +
+            'left: auto;' +
+            'top: 20%;' +
+            'line-height: 5em;' +
+            'padding: 10px;' +
+            'background-color: #5d5d5d;' +
+            'border: 2px solid #000;' +
             '}' +
-            '' +
-            'div.popover div.prompt div#input_form img.btn {' +
-            'margin-bottom: 0;' +
-            'height: 30px;' +
-            'width: auto;' +
-            'display: inline-block;' +
-            '}' +
-            '' +
-            'div.popover div.prompt label {' +
-            'overflow: auto;' +
-            '}' +
-            '' +
-            'div.popover div.prompt input[type="radio"] {' +
-            'display: none;' +
-            '}' +
-            '' +
-            'div.popover div.prompt input[type="radio"] + label:before {' +
-            'display: inline-block;' +
-            'content: "";' +
-            'float: left;' +
-            'background: url(files/theme/popover_checkbox_unchecked.png) top;' +
-            'background-size: 30px 30px;' +
-            'height: 30px;' +
-            'width: 30px;' +
-            '}' +
-            '' +
-            'div.popover div.prompt input[type="radio"]:checked + label:before {' +
-            'content: "";' +
-            'background: url(files/theme/popover_checkbox_checked.png);' +
-            'background-size: 30px 30px;' +
-            'height: 30px;' +
-            'width: 30px;' +
-            '}' +
-            '' +
-            'div.popover div.prompt img#button_submit {' +
-            'margin-bottom: 0;' +
             '}' +
             '</style>';
     }
